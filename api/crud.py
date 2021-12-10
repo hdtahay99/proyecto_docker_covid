a63f1afc801b4544805d56a0a986e9e9a768c638
Ramón Escobar <ramon.escobarm97@gmail.com>
2021-12-09 23:07:30 -0600

    mejoras en API

--- api/crud.py
+++ api/crud.py
@@ -1,4 +1,6 @@
 from sqlalchemy.orm import Session
+from sqlalchemy import and_
+from sqlalchemy import func
 
 from api.schemas import CovidValue
 from datetime import date
@@ -10,3 +12,16 @@ def get_covid_values(db: Session, skip: int = 0, limit: int = 100):
 
 def get_covid_filter(db: Session, skip: int = 0, limit: int = 100, status: str = 'Deaths'):
     return db.query(models.CovidValue).filter(models.CovidValue.status == status).offset(skip).limit(limit).all()
+
+def get_data_covid_test(db: Session, date, status: str = 'Deaths'):
+    return db.query(models.CovidValue).filter(models.CovidValue.date==date,
+                                              models.CovidValue.status==status
+                                              ).all()
+
+
+
+def get_data_covid_test2(db: Session, date_from: date,date_to: date, status: str = 'Deaths'):
+    return db.query(models.CovidValue).filter(and_(models.CovidValue.date >= date_from,
+                                                   models.CovidValue.date <= date_to),
+                                              models.CovidValue.status==status
+                                              ).all()
--- api/main.py
+++ api/main.py
@@ -4,7 +4,7 @@ from typing import List
 
 from fastapi import Depends, FastAPI, HTTPException
 from sqlalchemy.orm import Session
-
+from datetime import date
 from . import crud, models, schemas
 from .database import SessionLocal, engine
 
@@ -35,4 +35,14 @@ def read_covid_values(skip: int = 0, limit: int = 100, db: Session = Depends(get
 @app.get("/covid/values/filter", response_model=List[schemas.CovidValue])
 def read_covid_values(skip: int = 0, limit: int = 100, status: str = 'Deaths', db: Session = Depends(get_db)):
     res = crud.get_covid_filter(db, skip=skip, limit=limit, status=status)
+    return res
+
+@app.get("/covid/values/test1", response_model=List[schemas.CovidValue])
+def read_data_covid_test(date: date, status: str = 'Deaths', db: Session = Depends(get_db)):
+    res = crud.get_data_covid_test(db, date=date, status=status)
+    return res
+
+@app.get("/covid/values/test2", response_model=List[schemas.CovidValue])
+def read_data_covid_test2(date_from: date,date_to: date, status: str = 'Deaths', db: Session = Depends(get_db)):
+    res = crud.get_data_covid_test2(db, date_from=date_from,date_to=date_to, status=status)
     return res
