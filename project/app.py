e76d1ca90f34df14fb9c62f044cfab6fa9f252c8
Ramón Escobar <ramon.escobarm97@gmail.com>
2021-12-16 01:07:40 -0600

    Agragando graphs con info acumulada, formato a metrics

--- project/app.py
+++ project/app.py
@@ -12,13 +12,13 @@ st.set_page_config(layout='wide')
 # FILTER #
 ###########
 fecha1 = st.sidebar.date_input("Fecha inicial",
-                               date(2021, 3, 1)
+                               date(2021, 3, 1),
+
                                )
 fecha2 = st.sidebar.date_input("Fecha final",
                                date(2021, 6, 30)
                                )
 components.html("<center> <h1>COVID 19 en el mundo</h1></center><br><center><b>Casos acumulados entre el "+ str(fecha1)  +" y el "+ str(fecha2) +"</b></center>")
-
 
 def get_covid_values(skip = 0, limit = 100):
     response = requests.get(url = f"http://fastapi:8585/covid/values?skip={skip}&limit={limit}")
@@ -44,21 +44,12 @@ country = st.sidebar.multiselect("Selecciona un país",countries, default=defaul
 full_data = pd.DataFrame.from_records(get_covid_filter2(fecha1, fecha2)).reset_index()
 
 #### Muertes
-#df_muertes = pd.DataFrame.from_records(get_covid_filter2(fecha1, fecha2)).reset_index()
-#df_muertes = df_muertes[df_muertes.country_region.isin(country)]
-
 df_muertes = full_data[full_data.country_region.isin(country)]
 
 #### Confirmados
-#df_confirmed = pd.DataFrame.from_records(get_covid_filter2(fecha1, fecha2)).reset_index()
-#df_confirmed = df_confirmed[df_confirmed.country_region.isin(country)]
-
 df_confirmed = full_data[full_data.country_region.isin(country)]
 
 #### Recuperados
-#df_recovered = pd.DataFrame.from_records(get_covid_filter2(fecha1, fecha2)).reset_index()
-#df_recovered = df_recovered[df_recovered.country_region.isin(country)]
-
 df_recovered = full_data[full_data.country_region.isin(country)]
 
 
@@ -88,13 +79,13 @@ df_recovered_acum['Text'] =  "<b>Pais</b>: " + df_recovered_acum['country_region
                            '<br><b>Casos recuperados</b>:' + (df_recovered_acum['recovered']).astype(str)
 
 
-######################################
-### Preparando datos para metricas ###
-######################################
+#########################################################
+### Preparando datos para metricas y grafico acumulado###
+#########################################################
 
-#mtc_deaths_ld = df_muertes.where(datetime(df_muertes['date']) >= fecha2)
-#mtc_recovered_ld = df_recovered[[df_recovered.date == fecha2]]
-#mtc_confirmed_ld = df_confirmed[[df_confirmed.date == fecha2]]
+cumsum_confirmed = df_confirmed.groupby(['country_region', 'date']).sum().groupby(level=0).cumsum().reset_index()
+cumsum_recovered = df_recovered.groupby(['country_region', 'date']).sum().groupby(level=0).cumsum().reset_index()
+cumsum_death = df_muertes.groupby(['country_region', 'date']).sum().groupby(level=0).cumsum().reset_index()
 
 ### Mapa acumuladas del filtro ###
 
@@ -141,7 +132,8 @@ def line_cases(df, status ='Casos', val='confirmed'):
             x = group['date'],
             y = group[val],
             mode = 'lines',
-            name = country_region
+            name = country_region,
+            hovertemplate ="<b>%s</b><br><b>%s:</b>%%{y}"%(country_region, status)
         ))
 
     fig.update_layout(
@@ -157,10 +149,15 @@ def line_cases(df, status ='Casos', val='confirmed'):
 
     return fig
 
+## Data diaria
+line_confirmed = line_cases(df_confirmed, 'Casos confirmados', 'confirmed')
+line_recovered = line_cases(df_recovered, 'Pacientes recuperados', 'recovered')
+line_deaths = line_cases(df_muertes, "Fallecidos", 'deaths')
 
-line_confirmed = line_cases(df_confirmed, 'Casos confirmados diarios por pais', 'confirmed')
-line_recovered = line_cases(df_recovered, 'Pacientes recuperados diarios por pais', 'recovered')
-line_deaths = line_cases(df_muertes, "Fallecidos diarios por pais", 'deaths')
+## Data acumulada
+cumsum_line_confirmed = line_cases(cumsum_confirmed, 'Casos confirmados', 'confirmed')
+cumsum_line_recovered = line_cases(cumsum_confirmed, 'Casos recuperados', 'recovered')
+cumsum_line_deaths = line_cases(cumsum_death, 'Fallecidos', 'deaths')
 
 ###########
 #DASHBOARD#
@@ -175,29 +172,32 @@ m1, m2, m3, m4 = metrics.columns(4)
 
 with m1:
     st.metric(
-        label='Paises',
+        label='Paises seleccionados',
         value=len(country)
     )
 
+
 with m2:
     st.metric(
-        label='Fallecidos en el periodo',
-        value=sum(df_muertes_acum['deaths'])
-        #delta= sum(mtc_deaths_ld['value']),
-        #delta_color= 'inverse'
+        label='Casos confirmados en el periodo',
+        value= "{:,}".format(sum(df_confirmed_acum['confirmed']))
     )
 
 with m3:
     st.metric(
         label='Casos confirmados en el periodo',
-        value=len(country)
+        value= "{:,}".format(sum(df_recovered_acum['recovered']))
     )
 
 with m4:
     st.metric(
-        label='Paises',
-        value=len(country)
+        label='Fallecidos en el periodo',
+        value="{:,}".format(sum(df_muertes_acum['deaths']))
+        #delta= sum(mtc_deaths_ld['value']),
+        #delta_color= 'inverse'
     )
+
+
 
 
 ##### MAPAS #####
@@ -218,14 +218,32 @@ with map3:
 
 ##### DE LINEAS #####
 
+conf1, conf2 = st.columns(2)
+rec1, rec2 = st.columns(2)
+dead1, dead2 = st.columns(2)
 
-with st.expander("Confirmados"):
+### Confirmados
+
+with conf1:
     st.plotly_chart(line_confirmed, use_container_width=True)
 
-with st.expander("Recuperados"):
+with conf2:
+    st.plotly_chart(cumsum_line_confirmed, use_container_width=True)
+
+#### Recuperados
+
+with rec1:
     st.plotly_chart(line_recovered, use_container_width=True)
 
-with st.expander("Fallecidos"):
+with rec2:
+    st.plotly_chart(cumsum_line_recovered, use_container_width=True)
+
+##### Fallecidos
+
+with dead1:
     st.plotly_chart(line_deaths, use_container_width=True)
 
-##### DE LINEAS #####
+with dead2:
+    st.plotly_chart(cumsum_line_deaths, use_container_width=True)
+
+##### Tablas
